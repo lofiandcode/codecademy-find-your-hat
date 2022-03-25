1a9e882a3106d3321bfacd7a2d0003a85920b0d0
lofiandcode <josephtrettevik@gmail.com>
2022-03-24 19:37:56 -0700

    Create updateCurrentPosition function

--- modules/functions.js
+++ modules/functions.js
@@ -1,16 +1,36 @@
-const Field = require('./classes.js');
 
-const myField = new Field([
-    ['*', '░', 'O','░'],
-    ['░', '░', '░','░'],
-    ['░', 'O', '░','O'],
-    ['░', '░', '░','░'],
-    ['░', '^', '░','░'],
-]);
-
-const printBoard = () => {
-    myField.print();
-    console.log('Which way do you want to move? ')
+const printBoard = (currentField) => {
+    currentField.print();
 }
 
-module.exports = { printBoard };
+const updateCurrentPosition = (direction, position) => {
+    switch (direction) {
+        case 'u':
+            position[0] -= 1;
+            break;
+        case 'd':
+            position[0] += 1;
+            break;
+        case 'l':
+            position[1] -= 1;
+            break;
+        case 'r':
+            position[1] += 1;
+            break;
+        default:
+            console.log('Error: Invalid entree');
+    }
+}
+
+// const nextMove = () => {
+//     process.stdout.write('Which way do you want to move? ');
+//     process.stdin.on('data', updateBoard);
+// } 
+
+// const updateBoard = (userInput) => {
+//     let input = userInput.toString().trim();
+//     process.stdout.write(input);
+//     process.exit();
+// }
+
+module.exports = { printBoard, updateCurrentPosition };
