6d72c29a5e7c9497fd4529b583ba40397ef33f79
lofiandcode <josephtrettevik@gmail.com>
2022-03-24 19:37:56 -0700

    Create updateCurrentPosition function

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 const prompt = require('prompt-sync')({sigint: true});
-const { printBoard } = require('./modules/functions');
+const { printBoard, updateCurrentPosition } = require('./modules/functions');
+const Field = require('./modules/classes.js');
 
+const myField = new Field([
+    ['*', '░', 'O','░'],
+    ['░', '░', '░','░'],
+    ['░', 'O', '░','O'],
+    ['░', '░', '░','░'],
+    ['░', '^', '░','░'],
+]);
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+let currentPosition = [0, 0];
+//u = up
+//d = down
+//l = left
+//r = right
 
 const main = () => {
-    printBoard();
+    printBoard(myField);
+    // nextMove();
+    //prompt next move?
+    let n = prompt('Next move? ');
+    currentPosition = updateCurrentPosition(n, currentPosition);
+    console.log('Current position is: ', currentPosition);
 }
 
 main();
